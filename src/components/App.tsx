d32e1aaa6165ab253c3dbe93a6cc13b06b422040
Lashi <lashi0812@gmail.com>
2024-09-23 10:30:33 +0000

    used children prop and typed

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,14 +1,59 @@
+import { useState, useEffect } from "react";
 import Background from "./Background";
 import Container from "./Container";
 import Footer from "./Footer";
-import Header from "./Header";
+import Header, { HeaderTop } from "./Header";
+import BookmarksButton from "./BookmarksButton";
+import Logo from "./Logo";
+import SearchForm from "./SearchForm";
+import JobItemContent from "./JobItemContent";
+import Sidebar, { SidebarTop } from "./Sidebar";
+import JobList from "./JobList";
+import PaginationControls from "./PaginationControls";
+import ResultsCount from "./ResultsCount";
+import SortingControls from "./SortingControls";
 
 function App() {
+  const [searchText, setSearchText] = useState("");
+  const [jobItems, setJobItems] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+
+  useEffect(() => {
+    if (searchText.length === 0) return;
+    setIsLoading(true);
+    const fetctData = async () => {
+      const response = await fetch(
+        `https://bytegrad.com/course-assets/projects/rmtdev/api/data?search=${searchText}`
+      );
+      const data = await response.json();
+      setJobItems(data.jobItems);
+      setIsLoading(false);
+    };
+
+    fetctData();
+  }, [searchText]);
+
   return (
     <>
       <Background />
-      <Header />
-      <Container />
+      <Header>
+        <HeaderTop>
+          <Logo />
+          <BookmarksButton />
+        </HeaderTop>
+        <SearchForm setSearchText={setSearchText} searchText={searchText} />
+      </Header>
+      <Container>
+        <Sidebar>
+          <SidebarTop>
+            <ResultsCount />
+            <SortingControls />
+          </SidebarTop>
+          <JobList jobItems={jobItems} isLoading={isLoading} />
+          <PaginationControls />
+        </Sidebar>
+        <JobItemContent />
+      </Container>
       <Footer />
     </>
   );
